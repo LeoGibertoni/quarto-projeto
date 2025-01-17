aee93afd7e4a95d01cd11ecbe527ee0ebfb1038a
Leonardo Gibertoni <gibertonileo@gmail.com>
2025-01-17 09:46:41 -0300

    att

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 export class AppComponent {
   workDurationMinutes: number = 25; // Definido como 25 minutos por padrão
   workDuration: number = this.workDurationMinutes * 60; // Convertendo para segundos
-  shortBreak: number = 5 * 60; // 5 minutos
-  longBreak: number = 15 * 60; // 15 minutos
+  shortBreak: number = 60 * 5; // 5 minutos
+  longBreak: number = 60 * 15; // 15 minutos
   timer: any;
   timeLeft: number = this.workDuration;
   currentSession: string = 'Trabalho';
@@ -19,7 +19,8 @@ export class AppComponent {
 
   timerDisplay: string = this.formatTime(this.timeLeft);
 
-  @ViewChild('alarmSound') alarmSound!: ElementRef;
+  @ViewChild('alarmSound', { static: false }) alarmSound!: ElementRef;
+
 
   formatTime(seconds: number): string {
     const minutes = Math.floor(seconds / 60).toString().padStart(2, '0');
@@ -27,26 +28,29 @@ export class AppComponent {
     return `${minutes}:${secs}`;
   }
 
-  // Método para definir o tempo de trabalho conforme o valor inserido pelo usuário
   setWorkTime() {
     if (this.workDurationMinutes > 0) {
-      this.workDuration = this.workDurationMinutes * 60; // Convertendo para segundos
+      this.workDuration = this.workDurationMinutes * 60;
       this.timeLeft = this.workDuration;
       this.timerDisplay = this.formatTime(this.timeLeft);
     }
   }
 
   startTimer() {
+    this.alarmSound.nativeElement.play().catch((error: any) => {
+      console.log('Autoplay bloqueado. Interação necessária.', error);
+    });
     this.isRunning = true;
     this.isPaused = false;
+
     this.timer = setInterval(() => {
       if (this.timeLeft > 0) {
         this.timeLeft--;
         this.timerDisplay = this.formatTime(this.timeLeft);
       } else {
-        clearInterval(this.timer); // Para o temporizador quando o tempo acaba
-        this.alarmSound.nativeElement.play(); // Toca o som do alarme
-        this.handleSessionSwitch(); // Alterna para a próxima sessão e ajusta o contador
+        clearInterval(this.timer);
+        this.alarmSound.nativeElement.play(); // Reproduz o som
+        this.handleSessionSwitch();
       }
     }, 1000);
   }
@@ -76,7 +80,6 @@ export class AppComponent {
       this.workSessions++; // Incrementa a sessão de trabalho
       console.log('Work session completed. Total:', this.workSessions);
 
-      // Alterna para pausa curta ou longa, dependendo do número de sessões
       if (this.workSessions % 4 === 0) {
         this.currentSession = 'Pausa Longa'; // A cada 4 sessões de trabalho, vai para a pausa longa
         this.timeLeft = this.longBreak;
@@ -85,28 +88,27 @@ export class AppComponent {
         this.timeLeft = this.shortBreak;
       }
     } else {
-      this.currentSession = 'Work';
+      this.currentSession = 'Trabalho';
       this.timeLeft = this.workDuration;
     }
 
     this.timerDisplay = this.formatTime(this.timeLeft);
 
-    // Aqui, o startTimer() deve ser chamado após definir o tempo correto para a nova sessão
-    this.startTimer(); // Inicia o temporizador para a nova sessão
+    this.startTimer();
   }
 
 
   addFiveMinutes() {
-    this.timeLeft += 5 * 60; // Adiciona 5 minutos em segundos
-    this.timerDisplay = this.formatTime(this.timeLeft); // Atualiza a exibição do cronômetro
+    this.timeLeft += 5 * 60;
+    this.timerDisplay = this.formatTime(this.timeLeft);
   }
 
   removeFiveMinutes() {
     if (this.timeLeft > 5 * 60) {
-      this.timeLeft -= 5 * 60; // Remove 5 minutos em segundos
+      this.timeLeft -= 5 * 60;
     } else {
-      this.timeLeft = 0; // Garante que o tempo não fique negativo
+      this.timeLeft = 0;
     }
-    this.timerDisplay = this.formatTime(this.timeLeft); // Atualiza a exibição do cronômetro
+    this.timerDisplay = this.formatTime(this.timeLeft);
   }
 }
